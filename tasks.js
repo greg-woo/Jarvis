879cc80b7107efa4717b9a8d9a21265f63aecf3e
greg-woo <35710259+greg-woo@users.noreply.github.com>
2021-01-30 17:49:28 -0500

    Time is now updated automatically

--- tasks.js
+++ tasks.js
@@ -8,4 +8,23 @@ $(document).ready(function() {
     }
     
     });
+
+    // $('input[id=currentDate]').change(function() {
+    
+    //     var today = new Date();
+    //     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    //     return date;
+        
+    //     });
+
     });
+
+//     localStorage.setItem("mytime", Date.now());
+
+//     <input type="text" id="currentDate">
+// <br><br>
+// <script>
+//   var today = new Date();
+//   var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+//   document.getElementById("currentDate").value = date;
+// </script>
