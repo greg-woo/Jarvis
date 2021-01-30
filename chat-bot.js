1ffb1d1eb184bb67d6b9bec3b4b518363c7eec3f
Jeremi <jeremi.dodinh@gmail.com>
2021-01-30 23:27:16 +0100

    added a scheduling functionality when help is called

--- chat-bot.js
+++ chat-bot.js
@@ -8,6 +8,17 @@ var messages = [], //array that hold the record of each string in chat
   botMessage = "", //var keeps track of what the chatbot is going to say
   botName = 'Chatbot', //name of the chatbot
   talking = true; //when false the speach function doesn't work
+
+var Calendar = {
+  "Jack": {
+    "2021-01-30": [{ title: "Daily Scrum meeting", startTime: "16:00", endTime: "16:15" }, { title: "Lunch and learn", startTime: "12:00", endTime: "13:00" }],
+    "2021-01-31": [{ title: "Project presentation", startTime: "9:00", endTime: "11:00" }]
+  },
+  "Jill": {
+    "2021-01-30": [{ title: "Coffee with Carl", startTime: "11:00", endTime: "11:30" }],
+    "2021-01-31": [{ title: "Deadline for report numbers", startTime: "16:00", endTime: "16:15" }]
+  }
+}
 //
 //
 //****************************************************************
@@ -22,14 +33,55 @@ function chatbotResponse() {
   talking = true;
   botMessage = "I'm confused"; //the default message
 
-  if (lastUserMessage === 'hi' || lastUserMessage =='hello') {
-    const hi = ['hi','howdy','hello']
-    botMessage = hi[Math.floor(Math.random()*(hi.length))];;
+  if (lastUserMessage.toLowerCase() === 'hi' || lastUserMessage.toLowerCase() == 'hello') {
+    const hi = ['hi', 'howdy', 'hello']
+    botMessage = hi[Math.floor(Math.random() * (hi.length))];;
   }
 
   if (lastUserMessage === 'name') {
     botMessage = 'My name is ' + botName;
   }
+  if (lastUserMessage === 'help') {
+    botMessage = ScheduleProvider();
+    botMessage = ScheduleProvider()
+  }
+  console.log(botMessage);
+}
+
+function ScheduleProvider() {
+  var available_people = []
+  const today = new Date()
+  const str_time = today.toJSON().toString().substring(11, 16)
+  const str_day = today.toJSON().toString().substring(0, 10)
+  for (var usr in Calendar) {
+    for (var day in Calendar[usr]) {
+      if (day === str_day) {
+        for (var event in Calendar[usr][str_day]) {
+          if (Date.parse("01/01/2011 " + Calendar[usr][str_day][0]["endTime"]) < Date.parse("01/01/2011 " + str_time) || (Date.parse("01/01/2011 " + Calendar[usr][str_day2][0]["startTime"]) > 90000 + Date.parse("01/01/2011 " + str_time))) {
+            // available_people.push(usr);
+            break;
+          }
+        }
+      }
+    }
+  }
+  response = ""
+  if (available_people.length>1) {
+    response += "It seems like "
+    for (var i = 0; i< available_people.length - 1; i++) {
+      response += available_people[i] + ", "
+    }
+    response += "and " + available_people[available_people.length - 1] + " are available right now. Do you want to send one of them a message?"
+  }
+  if (available_people.length == 1) {
+    response += "It seems like " + available_people[0] + " is available right now. Do you want to send them a message?"
+  }
+  if (available_people.length == 0) {
+    response += "Unfortunately, it seems like no one is available right now... why not take a little coffee break to think it over?"
+  }
+  console.log(response);
+  return response;
+
 }
 //****************************************************************
 //****************************************************************
@@ -95,7 +147,7 @@ function keyPress(e) {
   }
   if (key == 38) {
     console.log('hi')
-      //document.getElementById("chatbox").value = lastUserMessage;
+    //document.getElementById("chatbox").value = lastUserMessage;
   }
 }
 
